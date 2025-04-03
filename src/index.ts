c86f01aa323c9c4f6df5e88ac4878a2b8801d78a
Your Namedwadwadadfwadwadhawhjduwahdgit config --global user.email you@example.comgit config --global user.name Your <drohi2026@gmail.com>
2025-04-03 19:27:16 -0400

    update

--- src/index.ts
+++ src/index.ts
@@ -4,41 +4,19 @@ const data: Data = loadJSON("../DO_NOT_TOUCH/data.json") as Data; //Don't delete
 
 const pokedex : Pokedex = loadJSON("../DO_NOT_TOUCH/pokedex.json") as Pokedex; // Don't delete.
 
-// Merge sort
-function mergeSort(dataArray : number[], l : number, m : number, r : number) : number[]{
-
-
-
-  return dataArray;
-}
+class Algorithms{
   
-<<<<<<< HEAD
-=======
-  const data: Data = loadJSON("../DO_NOT_TOUCH/data.json") as Data; //Don't delete this line. All your data is here.
-  
-  interface Pokedex{
-    ids: number[];
-    names: string[];
-    types: string[];
-    stats: number[];
-    species: string[];
-    descriptions: string[];
-    evolutions_prev: string[];
-    evolutions_next: string[];
-    heights: number[];
-    weights: number[];
-    abilities: string[];
-    genders: number[];
-    images: string[];
-  }
-
-  const pokedex : Pokedex = loadJSON("../DO_NOT_TOUCH/pokedex.json") as Pokedex; // DONT DELETE BAKAS
-
   // Merge sort
-  function mergeSort(dataArray : number[], l : number, m : number, r : number) : number[]{
-    
+  public mergeSort(dataArray : number[], l : number, m : number, r : number) : number[]{
+
+
+
     return dataArray;
   }
+    
+  public merge(){
+
+  }
   
->>>>>>> b8f4087a67f287db30210b2680a30926c8695adf
-  
+}
+
