2506e7fd44cf06fc9334efcbf03d329d0cb3f802
Your Namedwadwadadfwadwadhawhjduwahdgit config --global user.email you@example.comgit config --global user.name Your <drohi2026@gmail.com>
2025-04-06 16:22:51 -0400

    utility functions

--- src/index.ts
+++ src/index.ts
@@ -53,17 +53,10 @@ function displayPokedex(pokedex: Pokedex) {
   }
 }
 
-// converts indexes to a data set based on order
-// so if [1, 3, 2, 4]
-// it will return the data value of [data[1], data[3], data[2], data[4]]
-function indexToData(indexes : number[], arr : any[]) : Array<any> {
-  let result : Array<any> = new Array(indexes.length);
-  for(let i=0; i<indexes.length; i++){
-      result[i] = arr[indexes[i]];
-  }
-  return result;
-}
 
+
+/* HOW TO USE BINARY SEARCH AND MERGE SORT
+// they have to be opposite
 let t : MergeSortLL<number> = new MergeSortLL(data.pokemonId);
 console.log(data.pokemonId);
 let m = (t.sort(ascending));
@@ -73,3 +66,4 @@ console.log(v);
 let d = binarySearch(1, v, desending);
 console.log(d);
 console.log(indexToData(d, v));
+*/
