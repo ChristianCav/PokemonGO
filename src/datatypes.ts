3668de296b2043c67eba71da0c27dc9b54387ce1
Your Namedwadwadadfwadwadhawhjduwahdgit config --global user.email you@example.comgit config --global user.name Your <drohi2026@gmail.com>
2025-04-06 14:49:17 -0400

    small changes format

---
+++ src/datatypes.ts
@@ -0,0 +1,24 @@
+// data classes for different data
+
+interface Data {
+    localTime: string[];
+    pokemonId: string[];
+    longitude: number[];
+    latitude: number[];
+}  
+
+interface Pokedex{
+    ids: number[];
+    names: string[];
+    types: string[];
+    stats: number[];
+    species: string[];
+    descriptions: string[];
+    evolutions_prev: string[];
+    evolutions_next: string[];
+    heights: number[];
+    weights: number[];
+    abilities: string[];
+    genders: number[];
+    images: string[];
+}
