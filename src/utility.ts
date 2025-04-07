089261799f4434bdbb970738544ee6d6f4b8a1e8
ChristianCav <christiancav55@gmail.com>
2025-04-07 12:12:20 -0400

    decimal functions

--- src/utility.ts
+++ src/utility.ts
@@ -9,4 +9,19 @@ function indexToData(indexes : number[], arr : any[]) : Array<any> {
         result[i] = arr[indexes[i]];
     }
     return result;
-  }
+}
+
+// returns number of decimal places
+// takes number as a string as input Ex: "1.23"
+function numDecimals(input: string): number{
+    // If no period, there are no decimals
+    if(!input.includes('.')) return 0;
+    // Split the string into two parts, from before and after the decimal
+    // Access the part after the decimal using [1]
+    // Return the number of digits after the decimal
+    return input.split('.')[1].length;  
+}
+
+function roundToDecimal(input: number, numDecimals: number): number{
+    return Number(input.toFixed(numDecimals));
+}
